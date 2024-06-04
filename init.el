122b371967291c1fc23346129798e8abaa34ba97
Aroldo Souza-Leite <asouzaleite@gmx.de>
2024-06-04 11:28:50 +0200

    after installing straight.el and using early-init.el

--- init.el
+++ init.el
@@ -1,18 +1,13 @@
-(message "user Emacs directory: %s" user-emacs-directory)
-
-;; ;;;<straight.el>
-(load (concat user-emacs-directory "straight.el/" "bootstrap.el"))
-;; ;;;</straight.el>
-
-(load (concat user-emacs-directory "user-config.el"))
-;; ;;;</general>
-
+(require 'straight)
+(straight-use-package 'f)
+(require 'f)
+(require 'cl-lib)
 
 ;; ;;; <layers>
-(let*((layers-dir (concat user-emacs-directory "layers/"))
-      (layers '("asl/"))
+(let*((layers-dir (f-join user-emacs-directory "layers"))
+      (layers '("asl"))
       )
   (cl-loop for layer in layers do
 	   (progn (
-		 load (concat layers-dir layer  "packages.el")))))
+		 load (f-join layers-dir layer  "packages.el")))))
 ;; ;;; </layers>
